119dfdba4e3f14cdc40121e580c8698a76115163
Denis8871 <145900110+Denis8871@users.noreply.github.com>
2024-06-16 17:56:41 +0300

    AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab 22 Dykyi")]
+[assembly: AssemblyTitle("Lab 22 Shostya")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab 22 Dykyi")]
+[assembly: AssemblyProduct("Lab 22 Shostya")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
